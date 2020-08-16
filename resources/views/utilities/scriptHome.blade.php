c4fc23cd3905d838317b65062e5810bd1b95f1bd
Lianum Frada Bahesna <fbahezna@gmail.com>
2020-08-16 16:48:00 +0700

    adding ui homepage

--- resources/views/utilities/scriptHome.blade.php
+++ resources/views/utilities/scriptHome.blade.php
@@ -17,6 +17,7 @@
     }
 
     $('.loader').fadeOut();
+    
 
 // END LOADER
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,7 +49,7 @@
             </a>
           </div>
   
-          <div class="row mt-3" id="row-product">
+          {{-- <div class="row mt-3" id="row-product">
             @foreach($products["data"] as $product)
             <div class="col-lg-4 col-md-6 mb-4 jenis" data-aos="zoom-out-right">
               <div class="card h-100">
@@ -66,15 +66,16 @@
               </div>
             </div>
             @endforeach
-          </div>
-          <div class="row">
+          </div> --}}
+          {{-- <div class="row">
             <div class="col-md text-right">
               <a class="btn btn-md btn-outline-default btn-rounded btn-lainnya waves-effect rounded" data-aos="fade-left" href="{{ route('product.all') }}">Lainnya</a>
             </div>
-          </div>
+          </div> --}}
         </div>
     </div>
   </div>
+
   <div class="container-fluid mt-3 bg-light">
       <div class="row">
           <!--Grid column-->
@@ -125,5 +126,82 @@
         </div>
       </div>
   </div>
-    @endsection
+
+  <div class="container-fluid bg-white">
+    <div class="row">
+      <div class="col-md-8 offset-2">
+        <div class="row mt-3" id="row-product">
+          @foreach($products["data"] as $product)
+          <div class="col-lg-4 col-md-6 mb-4 jenis" data-aos="zoom-out-right">
+            <div class="card h-100">
+              <a href="{{ route('product.detail',["id" => $product["id"]]) }}"><img class="card-img-top" src="{{ url('storage/image/'.$product["gambar_product"]) }}" style="height: 200px" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="{{ route('product.detail',["id" => $product["id"]]) }}">{{ $product["nama_product"] }}</a>
+              </h4>
+              <h6>{{ "Rp " . number_format($product["harga_product"],2,',','.') }}</h6>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+             </div>
+          </div>
+        </div>
+         @endforeach
+        </div>
+          <div class="row">
+            <div class="col-md text-center">
+              <a class="btn btn-lg btn-outline-default btn-rounded btn-lainnya waves-effect rounded" data-aos="fade-left" href="{{ route('product.all') }}">Lainnya</a>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid bg-light">
+      <div id="carouselExampleControls" class="carousel slide p-3 mt-3" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="carousel-image">
+              <img data-aos="fade-right" class="d-block rounded img-rounded" height="200" width="200"  src="{{ asset('storage/testimoni/testi1.jpg') }}" alt="First slide">
+            </div>
+            <div class="carousel-caption d-none d-md-block text-dark">
+              <h3 data-aos="flip-right">Pak Julkipli</h3>
+              <h4 data-aos="flip-right">Petani</h4>
+              <p data-aos="flip-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Minus accusantium nihil corrupti deleniti quis ipsum, unde quaerat fuga ea 
+                ipsa perspiciatis cupiditate ut dolores nemo commodi impedit sit voluptate veniam!</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img data-aos="fade-right" class="d-block rounded img-rounded" height="200" width="200" src="{{ asset('storage/testimoni/testi2.jpg') }}" alt="Second slide">
+            <div class="carousel-caption d-none d-md-block text-dark">
+              <h3 data-aos="flip-right">Pak Diwid</h3>
+              <h4 data-aos="flip-right">Peternak Lele</h4>
+              <p data-aos="flip-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Minus accusantium nihil corrupti deleniti quis ipsum, unde quaerat fuga ea 
+                ipsa perspiciatis cupiditate ut dolores nemo commodi impedit sit voluptate veniam!</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img data-aos="fade-right" class="d-block rounded img-rounded" height="200" width="200" src="{{ asset('storage/testimoni/testi3.jpg') }}" alt="Third slide">
+            <div class="carousel-caption d-none d-md-block text-dark">
+              <h3 data-aos="flip-right">Pak diudik</h3>
+              <h4 data-aos="flip-right">Rentenir</h4>
+              <p data-aos="flip-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Minus accusantium nihil corrupti deleniti quis ipsum, unde quaerat fuga ea 
+                ipsa perspiciatis cupiditate ut dolores nemo commodi impedit sit voluptate veniam!</p>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+  </div>
+@endsection
 
